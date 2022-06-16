a9ef82cdb3e1b4dc406a74b430e7dacec8281091
Rana-Paul <ranapaul741223@gmail.com>
2022-06-16 23:58:01 +0530

    issue-3

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -2,8 +2,11 @@ var menu = document.getElementById('menu');
 
 var sideBar = document.querySelector(".sidebar");
 
+var catagoryBar = document.querySelector(".catagory");
+
 menu.onclick = function () {
-    
+
+    catagoryBar.classList.toggle("large-catagory-bar");
     sideBar.classList.toggle("small-sidebar");
 
 }
