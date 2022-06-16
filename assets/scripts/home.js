fab31344a093f26f8a1a42ad780f4af0517d974e
Rana Paul <72220579+Rana-Paul@users.noreply.github.com>
2022-06-16 17:23:26 +0530

    issue-2 (#19)

---
+++ assets/scripts/home.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById('menu');
+
+var sideBar = document.querySelector(".sidebar");
+
+menu.onclick = function () {
+    
+    sideBar.classList.toggle("small-sidebar");
+
+}
